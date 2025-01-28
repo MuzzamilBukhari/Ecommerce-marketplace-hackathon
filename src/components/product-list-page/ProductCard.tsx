168ee02064265b2862c319b9c07076879abe7090
Muhammad Muzzamil Bukhari <muhammadmuzzamilbukhari@gmail.com>
2025-01-28 19:34:10 +0500

    fix

--- src/components/product-list-page/ProductCard.tsx
+++ src/components/product-list-page/ProductCard.tsx
@@ -21,7 +21,7 @@ const ProductCard = ({
   return (
     <Link
       href={`/shop/${category}/${slug}`}
-      className="w-[88%] sm:w-auto text-black hover:scale-105 duration-200 shadow-myGry/40 shadow-md rounded-xl"
+      className=" text-black hover:scale-105 duration-200 shadow-myGry/40 shadow-md rounded-xl"
     >
       <div className=" flex justify-center items-center flex-col py-6 px-2 sm:p-5">
         <div className="w-[250px] h-[250px] overflow-hidden object-center object-cover">
